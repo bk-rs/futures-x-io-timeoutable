bfc1b2294e1b25a244f0a8899b0c78ba00ef2dcb
vkill <vkill.net@gmail.com>
2020-08-13 09:20:14 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# futures-x-io-timeoutable
+
+* [Cargo package](https://crates.io/crates/futures-x-io-timeoutable)
+
+## Dev
+
+```
+cargo clippy --all -- -D clippy::all && \
+cargo fmt --all -- --check
+```
+
+```
+cargo test-all-features
+```
---
+++ tests/rw_tokio_io.rs
@@ -0,0 +1,49 @@
+#[cfg(all(not(feature = "futures_io"), feature = "tokio_io"))]
+mod rw_tokio_io_tests {
+    use std::io;
+    use std::time::{Duration, Instant};
+
+    use tokio::net::{TcpListener, TcpStream};
+    use tokio::stream::StreamExt;
+
+    use futures_x_io_timeoutable::{AsyncReadWithTimeoutExt, AsyncWriteWithTimeoutExt};
+
+    #[tokio::test()]
+    async fn simple() -> io::Result<()> {
+        let mut listener = TcpListener::bind("127.0.0.1:0").await?;
+        let addr = listener.local_addr()?;
+
+        let mut tcp_stream_c = TcpStream::connect(addr).await?;
+        let mut tcp_stream_s = listener
+            .incoming()
+            .next()
+            .await
+            .expect("Get next incoming failed")?;
+
+        tcp_stream_s
+            .write_with_timeout(b"foo", Duration::from_secs(1))
+            .await?;
+
+        let mut buf = vec![0u8; 5];
+        let n = tcp_stream_c
+            .read_with_timeout(&mut buf, Duration::from_secs(1))
+            .await?;
+        assert_eq!(n, 3);
+        assert_eq!(buf, b"foo\0\0");
+
+        let instant = Instant::now();
+        let two_secs = Duration::from_secs(2);
+        let three_secs = Duration::from_secs(3);
+        let err = tcp_stream_c
+            .read_with_timeout(&mut buf, Duration::from_secs(2))
+            .await
+            .err()
+            .unwrap();
+        assert!(instant.elapsed() >= two_secs);
+        assert!(instant.elapsed() < three_secs);
+        assert_eq!(err.kind(), io::ErrorKind::TimedOut);
+        assert_eq!(err.to_string(), "read timeout");
+
+        Ok(())
+    }
+}
