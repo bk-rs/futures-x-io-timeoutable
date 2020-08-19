c7a74a054b968c2e7935373aa0a3bd11dbeeff52
vkill <vkill.net@gmail.com>
2020-08-19 20:30:23 +0800

    Add async-std and tokio demos

---
+++ demos/async-std/src/main.rs
@@ -0,0 +1,31 @@
+/*
+cargo run -p futures-x-io-timeoutable-demo-async-std
+*/
+
+use std::io;
+use std::time::Duration;
+
+use async_std::io::Cursor;
+
+use futures_x_io_timeoutable::AsyncReadWithTimeoutExt;
+
+#[async_std::main]
+async fn main() -> io::Result<()> {
+    run().await
+}
+
+async fn run() -> io::Result<()> {
+    //
+    let mut cursor = Cursor::new(b"foo".to_vec());
+
+    let mut buf = vec![0u8; 5];
+    let n = cursor
+        .read_with_timeout(&mut buf, Duration::from_secs(1))
+        .await?;
+    assert_eq!(n, 3);
+    assert_eq!(buf, b"foo\0\0");
+
+    println!("done");
+
+    Ok(())
+}
---
+++ demos/tokio/src/main.rs
@@ -0,0 +1,30 @@
+/*
+cargo run -p futures-x-io-timeoutable-demo-tokio
+*/
+
+use std::io;
+use std::io::Cursor;
+use std::time::Duration;
+
+use futures_x_io_timeoutable::AsyncReadWithTimeoutExt;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    run().await
+}
+
+async fn run() -> io::Result<()> {
+    //
+    let mut cursor = Cursor::new(b"foo".to_vec());
+
+    let mut buf = vec![0u8; 5];
+    let n = cursor
+        .read_with_timeout(&mut buf, Duration::from_secs(1))
+        .await?;
+    assert_eq!(n, 3);
+    assert_eq!(buf, b"foo\0\0");
+
+    println!("done");
+
+    Ok(())
+}
